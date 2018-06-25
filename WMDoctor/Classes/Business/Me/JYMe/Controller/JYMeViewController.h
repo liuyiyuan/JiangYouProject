e10851646b05d1a2a68ac09897877eb06aec7abe
jiangqi <jiangqi@jiangqideiMac.local>
2018-06-25 17:31:29 +0800

    修改首页崩溃问题  /  添加'我的'页面(待完善)

---
+++ WMDoctor/Classes/Business/Me/JYMe/Controller/JYMeViewController.h
@@ -0,0 +1,13 @@
+//
+//  JYMeViewController.h
+//  WMDoctor
+//
+//  Created by jiangqi on 2018/6/25.
+//  Copyright © 2018年 Choice. All rights reserved.
+//
+
+#import "WMBaseViewController.h"
+
+@interface JYMeViewController : WMBaseViewController
+
+@end
